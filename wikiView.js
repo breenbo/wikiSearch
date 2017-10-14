e4f50fb2e7301c49bc8b83c124519eeb50aecded
breenbo <berrehuelbruno@gmail.com>
2017-10-14 15:08:12 -0400

    some changes

--- wikiView.js
+++
@@ -1,8 +0,0 @@
-// $(".button").hover(
-    // function() {
-        // $(this).css({
-            // "box-shadow" : "0 1vw 1vw 0 rgba(0, 0, 0, 0.2), 0 1vw 1vw 0 rgba(0, 0, 0, 0.19)",
-            // "transition-duration":"0.1s"
-        // });
-    // }
-// )
